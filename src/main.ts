7b2478a884950eb7c5ad6380d3dc731d7fa9c1a9
m1sha <m1sha@live.ru>
2021-01-02 23:55:44 +0700

    [master] build & watch modes

--- src/main.ts
+++ src/main.ts
@@ -11,32 +11,42 @@ const parts = site.parts
 const content = site.content
 const resolver = new TemplateResolver()
 
-_.info(__.StartSiteAssembly)
-_.info(__.PublishPages)
-for (const page of pages) {
-  content.add(__.PageName, page.name)
-  try {
-    page.attach()
-    const layout = layouts.getLayout(page.layoutName)
-    const html = resolver.resolve(layout, page, parts, content)
-    
-    site.publishPage(page.name, html)
-    _.succ(__.page(page.name))
-  } catch(e){
-    _.err(__.page(page.name, e.message))
+if (process.env.watch){
+  const server = new DevServer()
+  server.start()
+} else {
+  _.info(__.StartSiteAssembly)
+  publishPages()
+  publishAssets()
+  _.info(__.EndSiteAssembly)
+}
+
+
+function publishPages(): void{
+  _.info(__.PublishPages)
+  for (const page of pages) {
+    content.add(__.PageName, page.name)
+    try {
+      page.attach()
+      const layout = layouts.getLayout(page.layoutName)
+      const html = resolver.resolve(layout, page, parts, content)
+      
+      site.publishPage(page.name, html)
+      _.succ(__.page(page.name))
+    } catch(e){
+      _.err(__.page(page.name, e.message))
+    }
   }
 }
 
-_.info(__.Assets)
-site.publishAssets((src, dist, err)=>{
-  if (err){
-    _.err(`${err}. ${src}`)
-    return
-  }
-  _.succ(`${dist}`)
-
-})
-_.info(__.EndSiteAssembly)
-
-const server = new DevServer()
-server.start()
+function publishAssets(): void{
+  _.info(__.Assets)
+  site.publishAssets((src, dist, err)=>{
+    if (err){
+      _.err(`${err}. ${src}`)
+      return
+    }
+    _.succ(`${dist}`)
+  
+  })
+}
